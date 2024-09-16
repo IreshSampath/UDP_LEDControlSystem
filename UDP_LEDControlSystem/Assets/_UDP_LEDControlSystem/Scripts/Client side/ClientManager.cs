766d06cf35f08c94072a4d9bedccb1edae1f965e
Iresh Sampath <ireshsampath@gmail.com>
2024-09-16 19:17:17 +0400

    Backup before tidy

--- UDP_LEDControlSystem/Assets/_UDP_LEDControlSystem/Scripts/Client side/ClientManager.cs
+++ UDP_LEDControlSystem/Assets/_UDP_LEDControlSystem/Scripts/Client side/ClientManager.cs
@@ -3,7 +3,6 @@ using Unity.Collections;
 using Unity.Networking.Transport;
 using System;
 using UnityEngine.UI;
-using System.Net;
 
 namespace GAG.UDPLEDControlSystem
 {
@@ -16,34 +15,55 @@ namespace GAG.UDPLEDControlSystem
 
         void Start()
         {
-            StartClient();
-        }
-
-        void StartClient()
-        {
-            _clientUIManager.PrintConsole("I'm Client");
-
-            _driver = NetworkDriver.Create();
-
-            var endpoint = NetworkEndpoint.LoopbackIpv4.WithPort(7777);
-            _connection = _driver.Connect(endpoint);
-
-        }
-
-        void OnDestroy()
-        {
-            _driver.Dispose();
+            InitClient();
         }
 
         void Update()
         {
+            UpdateClient();
+        }
+
+        void OnDestroy()
+        {
+            ShutdownClient();
+        }
+
+        // Initiate the networkDriver and connection
+        void InitClient()
+        {
+            _clientUIManager.PrintConsole("I'm Client");
+
+            _driver = NetworkDriver.Create();
+            _connection = default(NetworkConnection);
+
+            NetworkEndpoint endpoint = NetworkEndpoint.LoopbackIpv4.WithPort(7777);
+            _connection = _driver.Connect(endpoint);
+        }
+
+        void ShutdownClient()
+        {
+            _driver.Dispose();
+        }
+
+        void UpdateClient()
+        {
             _driver.ScheduleUpdate().Complete();
 
-            if (!_connection.IsCreated)
+            CheckAlive();
+            UpdateMessages();
+        }
+        
+        void CheckAlive()
+        {
+            if(!_connection.IsCreated)
             {
-                return;
-            }
+                _clientUIManager.PrintConsole("Somthing went wrong, lost connection to the server.");
 
+            }
+        }
+
+        void UpdateMessages()
+        {
             DataStreamReader stream;
             NetworkEvent.Type cmd;
 
@@ -54,9 +74,9 @@ namespace GAG.UDPLEDControlSystem
                     _clientUIManager.PrintConsole("We are now connected to the server.");
                     Debug.Log("We are now connected to the server.");
 
-                    uint value = 1;
+                    ulong value = 1;
                     _driver.BeginSend(_connection, out var writer);
-                    writer.WriteUInt(value);
+                    writer.WriteULong(value);
                     _driver.EndSend(writer);
                 }
                 else if (cmd == NetworkEvent.Type.Data)
@@ -65,49 +85,68 @@ namespace GAG.UDPLEDControlSystem
                     //print(value);
 
                     string tmp = value.ToString();
-                    //print(tmp);
+                    print(tmp);
+                    _clientUIManager.PrintConsole(tmp);
 
                     string ledID = value.ToString().Substring(0, 3);
                     print(ledID);
-                    //_clientUIManager.PrintConsole(ledID);
+                    _clientUIManager.PrintConsole(ledID);
 
-                    string colorToHex = value.ToString().Substring(3, tmp.Length - 3);
-                    print(colorToHex);
+                    string r = value.ToString().Substring(3,3);
+                    string g = value.ToString().Substring(6,3);
+                    string b = value.ToString().Substring(9,3);
+
+                    print("ID: " + ledID + " R: " + r + " G: " + g + " B: " + b);
+
+
+                    ///string colorToHex = value.ToString().Substring(4, tmp.Length - 5);
+                    //print(colorToHex);
                     //_clientUIManager.PrintConsole(colorToHex);
 
-                    string hexString = Int32.Parse(colorToHex).ToString("X");
-                    hexString = '#' + hexString;
+                    ///string hexString = Int64.Parse(colorToHex).ToString("X");
+                    ///hexString = '#' + hexString;
                     //string hexString = value.ToString("X");
-                    print(hexString);
+                    //print(hexString);
                     //_clientUIManager.PrintConsole(hexString);
 
-                    if (ledID.Substring(0, 2) == "10")
-                    {
-                        //_clientUIManager.PrintConsole(ledID);
-                        ledID = ledID.Substring(2);
+                    //if (ledID.Substring(0, 2) == "10")
+                    //{
+                    //    //_clientUIManager.PrintConsole(ledID);
+                    //    ledID = ledID.Substring(2);
 
-                        _clientUIManager.PrintConsole(ledID);
+                    //   // _clientUIManager.PrintConsole(ledID);
 
-                    }
-                    else if (ledID.Substring(0, 1) == "1")
-                    {
-                        //_clientUIManager.PrintConsole(ledID);
+                    //}
+                    //else if (ledID.Substring(0, 1) == "1")
+                    //{
+                    //    //_clientUIManager.PrintConsole(ledID);
 
-                        ledID = ledID.Substring(1, 2);
+                    //    ledID = ledID.Substring(1, 2);
 
-                        _clientUIManager.PrintConsole(ledID);
+                    //    //_clientUIManager.PrintConsole(ledID);
 
-                    }
+                    //}
 
-                    Color newColor = Color.black;
-                    Color myColor;
-                    if (ColorUtility.TryParseHtmlString(hexString, out myColor)){
-                        newColor = myColor;
-                    }
-                    else
-                    {
-                        newColor = Color.red;
-                    }
+                    ledID = GetLEDID(ledID);
+                    _clientUIManager.PrintConsole("ID: " + ledID);
+
+                    Color32 newColor = Color.black;
+
+                    newColor.r = (byte)GetRGBSubtrings(r);
+                    newColor.g = (byte)GetRGBSubtrings(g);
+                    newColor.b = (byte)GetRGBSubtrings(b);
+
+                    print(" R: " + newColor.r + " G: " + newColor.g + " B: " + newColor.b);
+
+                    //Color myColor;
+                    //if (ColorUtility.TryParseHtmlString(hexString, out myColor))
+                    //{
+                    //    newColor = myColor;
+                    //}
+                    //else
+                    //{
+                    //    newColor = Color.red;
+                    //}
 
                     foreach (GameObject lEDLight in _clientUIManager.LEDLights)
                     {
@@ -119,23 +158,153 @@ namespace GAG.UDPLEDControlSystem
                             lEDLight.GetComponent<Transform>().GetChild(0).GetComponent<Image>().color = newColor;
                         }
                     }
-
-                    Debug.Log($"Got the value {value} back from the server.");
-
-                     _connection.Disconnect(_driver);
-                     _connection = default;
-                    StartClient();
-
                 }
                 else if (cmd == NetworkEvent.Type.Disconnect)
                 {
                     _clientUIManager.PrintConsole("Client got disconnected from server.");
-                    Debug.Log("Client got disconnected from server.");
                     _connection = default;
 
-                    StartClient();
+                    InitClient();
                 }
             }
         }
+        
+        string GetLEDID(string id)
+        {
+            if (id.Substring(0, 2) == "10")
+            {
+                id = id.Substring(2);
+            }
+            else if (id.Substring(0, 1) == "1")
+            {
+                id = id.Substring(1, 2);
+            }
+            return id;
+        }
+
+        int GetRGBSubtrings(string value)
+        {
+            if (value.Substring(0, 2) == "99")
+            {
+                value = value.Substring(2);
+            }
+            else if (value.Substring(0, 1) == "9")
+            {
+                value = value.Substring(1, 2);
+            }
+            return Int32.Parse(value);
+        }
+
+        public void SendToServer(uint msg)
+        {
+            DataStreamWriter writer;
+            _driver.BeginSend(_connection, out writer);
+            writer.WriteUInt(msg);
+            _driver.EndSend(writer);
+        }
+
+        //void UpdatetClient()
+        //{
+        //    _driver.ScheduleUpdate().Complete();
+
+        //    if (!_connection.IsCreated)
+        //    {
+        //        return;
+        //    }
+
+        //    DataStreamReader stream;
+        //    NetworkEvent.Type cmd;
+
+        //    while ((cmd = _connection.PopEvent(_driver, out stream)) != NetworkEvent.Type.Empty)
+        //    {
+        //        if (cmd == NetworkEvent.Type.Connect)
+        //        {
+        //            _clientUIManager.PrintConsole("We are now connected to the server.");
+        //            Debug.Log("We are now connected to the server.");
+
+        //            uint value = 1;
+        //            _driver.BeginSend(_connection, out var writer);
+        //            writer.WriteUInt(value);
+        //            _driver.EndSend(writer);
+        //        }
+        //        else if (cmd == NetworkEvent.Type.Data)
+        //        {
+        //            ulong value = stream.ReadULong();
+        //            //print(value);
+
+        //            string tmp = value.ToString();
+        //            //print(tmp);
+
+        //            string ledID = value.ToString().Substring(0, 3);
+        //            print(ledID);
+        //            //_clientUIManager.PrintConsole(ledID);
+
+        //            string colorToHex = value.ToString().Substring(3, tmp.Length - 3);
+        //            print(colorToHex);
+        //            //_clientUIManager.PrintConsole(colorToHex);
+
+        //            string hexString = Int32.Parse(colorToHex).ToString("X");
+        //            hexString = '#' + hexString;
+        //            //string hexString = value.ToString("X");
+        //            print(hexString);
+        //            //_clientUIManager.PrintConsole(hexString);
+
+        //            if (ledID.Substring(0, 2) == "10")
+        //            {
+        //                //_clientUIManager.PrintConsole(ledID);
+        //                ledID = ledID.Substring(2);
+
+        //                _clientUIManager.PrintConsole(ledID);
+
+        //            }
+        //            else if (ledID.Substring(0, 1) == "1")
+        //            {
+        //                //_clientUIManager.PrintConsole(ledID);
+
+        //                ledID = ledID.Substring(1, 2);
+
+        //                _clientUIManager.PrintConsole(ledID);
+
+        //            }
+
+        //            Color newColor = Color.black;
+        //            Color myColor;
+        //            if (ColorUtility.TryParseHtmlString(hexString, out myColor)){
+        //                newColor = myColor;
+        //            }
+        //            else
+        //            {
+        //                newColor = Color.red;
+        //            }
+
+        //            foreach (GameObject lEDLight in _clientUIManager.LEDLights)
+        //            {
+
+        //                if (lEDLight.name == ledID)
+        //                {
+        //                    //_clientUIManager.PrintConsole("lEDLight.name == ledID");
+
+        //                    lEDLight.GetComponent<Transform>().GetChild(0).GetComponent<Image>().color = newColor;
+        //                }
+        //            }
+
+        //            Debug.Log($"Got the value {value} back from the server.");
+
+        //             _connection.Disconnect(_driver);
+        //             _connection = default;
+        //             //StartClient();
+
+        //        }
+        //        else if (cmd == NetworkEvent.Type.Disconnect)
+        //        {
+        //            _clientUIManager.PrintConsole("Client got disconnected from server.");
+        //            Debug.Log("Client got disconnected from server.");
+        //            _connection = default;
+
+        //            //StartClient();
+        //        }
+        //    }
+        //}
+
     }
 }
--- UDP_LEDControlSystem/Assets/_UDP_LEDControlSystem/Scripts/Server side/ServerManager.cs
+++ UDP_LEDControlSystem/Assets/_UDP_LEDControlSystem/Scripts/Server side/ServerManager.cs
@@ -13,19 +13,33 @@ namespace GAG.UDPLEDControlSystem
         NetworkDriver _driver;
         NativeList<NetworkConnection> _connections;
 
+        bool _isSendData = false;
+
         void Start()
         {
-            StartServer();
+            InitServer();
         }
 
-        void StartServer()
+        void Update()
+        {
+            UpdateServer();
+        }
+
+        void OnDestroy()
+        {
+            ShutdownServer();
+        }
+
+
+        // Initiate the networkDriver and connection list
+        void InitServer()
         {
             _serverUIManager.PrintConsole("I'm Server");
 
             _driver = NetworkDriver.Create();
-            _connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
 
-            var endpoint = NetworkEndpoint.AnyIpv4.WithPort(7777);
+            NetworkEndpoint endpoint = NetworkEndpoint.AnyIpv4.WithPort(7777);
+
             if (_driver.Bind(endpoint) != 0)
             {
                 _serverUIManager.PrintConsole("Failed to bind to port 7777.");
@@ -33,15 +47,197 @@ namespace GAG.UDPLEDControlSystem
                 return;
             }
             _driver.Listen();
+
+            _connections = new NativeList<NetworkConnection>(2, Allocator.Persistent);
         }
 
-        void OnDestroy()
+        void ShutdownServer()
         {
             if (_driver.IsCreated)
             {
                 _driver.Dispose();
                 _connections.Dispose();
             }
+        }
+
+        void UpdateServer()
+        {
+            if (_isSendData)
+            {
+                _driver.ScheduleUpdate().Complete();
+
+                CleanupConnections();
+                AcceptNewConnections();
+                UpdateMessages();
+            }
+        }
+
+        // Clean up connections.
+        void CleanupConnections()
+        {
+            for (int i = 0; i < _connections.Length; i++)
+            {
+               // _serverUIManager.PrintConsole("Cleanup Connections");
+
+                if (!_connections[i].IsCreated)
+                {
+                    _connections.RemoveAtSwapBack(i);
+                    i--;
+                }
+            }
+        }
+
+        // Accept new connections.
+        void AcceptNewConnections()
+        {
+            NetworkConnection c;
+            while ((c = _driver.Accept()) != default)
+            {
+                _connections.Add(c);
+               // _serverUIManager.PrintConsole("Accepted a connection.");
+                Debug.Log("Accepted a connection.");
+            }
+        }
+
+        public void UpdateMessages()
+        {
+            for (int i = 0; i < _connections.Length; i++)
+            {
+                //_serverUIManager.PrintConsole("3rd part");
+
+                //DataStreamReader stream;
+                NetworkEvent.Type cmd;
+                while ((cmd = _driver.PopEventForConnection(_connections[i], out var stream)) != NetworkEvent.Type.Empty)
+                {
+                    if (cmd == NetworkEvent.Type.Data)
+                    {
+                        ulong ulongSendNumber = stream.ReadUInt();
+                        foreach (GameObject selectedLED in _serverUIManager.SelectedLEDButtons)
+                        {
+                            Color32 selectedColor = selectedLED.transform.GetChild(0).GetComponent<Image>().color;
+                            string r = selectedColor.r.ToString();
+                            string g = selectedColor.g.ToString();
+                            string b = selectedColor.b.ToString();
+                            //string stringHexColor = ColorUtility.ToHtmlStringRGBA(selectedColor);
+                            ///string stringHexColor = ColorUtility.ToHtmlStringRGB(selectedColor);
+
+                            //print("ID: " + selectedLED.name + " Color: " + stringHexColor);
+                            print("ID: " + selectedLED.name + " R: " + r + " G: " + g + " B: " + b);
+                            //_serverUIManager.PrintConsole("ID: " + selectedLED.name + " Color: " + stringHexColor);
+
+                            ///ulong ulongHexColor = Convert.ToUInt64(stringHexColor, 16);
+
+                            string lEDNumber = selectedLED.name;
+                            if (lEDNumber.Length == 1)
+                            {
+                                lEDNumber = "10" + lEDNumber;
+                            }
+                            else if (lEDNumber.Length == 2)
+                            {
+                                lEDNumber = "1" + lEDNumber;
+                            }
+
+                            if (r.Length == 1)
+                            {
+                                r = "99" + r;
+                            }
+                            else if (r.Length == 2)
+                            {
+                                r = "9" + r;
+                            }
+
+
+                            if (g.Length == 1)
+                            {
+                                g = "99" + g;
+                            }
+                            else if (g.Length == 2)
+                            {
+                                g = "9" + g;
+                            }
+
+
+                            if (b.Length == 1)
+                            {
+                                b = "99" + b;
+                            }
+
+                            else if (b.Length == 2)
+                            {
+                                b = "9" + b;
+                            }
+
+
+                            //string bindedName = lEDNumber + ulongHexColor;
+                            string bindedName = lEDNumber + r + g + b;
+                            _serverUIManager.PrintConsole(bindedName.ToString());
+
+                            ulongSendNumber = Convert.ToUInt64(bindedName);
+                            _serverUIManager.PrintConsole(ulongSendNumber.ToString());
+                            _driver.BeginSend(NetworkPipeline.Null, _connections[i], out var writer);
+                            writer.WriteULong(ulongSendNumber);
+                            _driver.EndSend(writer);
+                        }
+                        _connections[i].Disconnect(_driver);
+                        _driver.ScheduleFlushSend(default).Complete();
+                        _isSendData = false;
+
+                    }
+                    else if (cmd == NetworkEvent.Type.Disconnect)
+                    {
+                        _serverUIManager.PrintConsole("Client disconnected from the server.");
+                        Debug.Log("Client disconnected from the server.");
+                        _connections[i] = default;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void OnData(DataStreamReader stream, int i)
+        {
+            //ulong ulongSendNumber = stream.ReadUInt();
+            foreach (GameObject selectedLED in _serverUIManager.SelectedLEDButtons)
+            {
+                ulong ulongSendNumber = stream.ReadULong();
+
+                Color selectedColor = selectedLED.transform.GetChild(0).GetComponent<Image>().color;
+                string stringHexColor = ColorUtility.ToHtmlStringRGB(selectedColor);
+
+                //print("ID: " + selectedLED.name + " Color: " + stringHexColor);
+                //_serverUIManager.PrintConsole("ID: " + selectedLED.name + " Color: " + stringHexColor);
+
+                //uint number0 = Convert.ToUInt32(stringHexColor, 16);
+                ulong ulongHexColor = Convert.ToUInt64(stringHexColor, 16);
+
+                string lEDNumber = selectedLED.name;
+                if (lEDNumber.Length == 1)
+                {
+                    lEDNumber = "10" + lEDNumber;
+                }
+                else if (lEDNumber.Length == 2)
+                {
+                    lEDNumber = "1" + lEDNumber;
+                }
+
+                string bindedName = lEDNumber + ulongHexColor;
+
+                ulongSendNumber = Convert.ToUInt64(bindedName);
+                //_serverUIManager.PrintConsole(ulongSendNumber.ToString());
+                _driver.BeginSend(NetworkPipeline.Null, _connections[i], out var writer);
+                writer.WriteULong(ulongSendNumber);
+                _driver.EndSend(writer);
+            }
+        }
+
+        //public void SendToClient(NetworkConnection connection, uint msg)
+        public void SendToClient()
+        {
+            _isSendData = true;
+            //DataStreamWriter writer;
+            //_driver.BeginSend(connection, out writer);
+            //writer.WriteUInt(msg);
+            //_driver.EndSend(writer);
         }
 
         public void SendClient()
@@ -51,7 +247,7 @@ namespace GAG.UDPLEDControlSystem
             // Clean up connections.
             for (int i = 0; i < _connections.Length; i++)
             {
-                _serverUIManager.PrintConsole("1st part" );
+                //_serverUIManager.PrintConsole("1st part");
 
                 if (!_connections[i].IsCreated)
                 {
@@ -64,7 +260,7 @@ namespace GAG.UDPLEDControlSystem
             NetworkConnection c;
             while ((c = _driver.Accept()) != default)
             {
-                _serverUIManager.PrintConsole("2nd part");
+                //_serverUIManager.PrintConsole("2nd part");
 
                 _connections.Add(c);
                 _serverUIManager.PrintConsole("Accepted a connection.");
@@ -73,23 +269,24 @@ namespace GAG.UDPLEDControlSystem
 
             for (int i = 0; i < _connections.Length; i++)
             {
-                _serverUIManager.PrintConsole("3rd part");
+                //_serverUIManager.PrintConsole("3rd part");
 
-                DataStreamReader stream;
+                //DataStreamReader stream;
                 NetworkEvent.Type cmd;
-                while ((cmd = _driver.PopEventForConnection(_connections[i], out stream)) != NetworkEvent.Type.Empty)
+                while ((cmd = _driver.PopEventForConnection(_connections[i], out var stream)) != NetworkEvent.Type.Empty)
                 {
                     if (cmd == NetworkEvent.Type.Data)
                     {
-                        //ulong ulongSendNumber = stream.ReadUInt();
+                        ulong ulongSendNumber = stream.ReadUInt();
                         foreach (GameObject selectedLED in _serverUIManager.SelectedLEDButtons)
                         {
-                            ulong ulongSendNumber = stream.ReadULong();
+                            //ulong ulongSendNumber = stream.ReadULong();
 
                             Color selectedColor = selectedLED.transform.GetChild(0).GetComponent<Image>().color;
+                            //string stringHexColor = ColorUtility.ToHtmlStringRGBA(selectedColor);
                             string stringHexColor = ColorUtility.ToHtmlStringRGB(selectedColor);
 
-                            //print("ID: " + selectedLED.name + " Color: " + stringHexColor);
+                            print("ID: " + selectedLED.name + " Color: " + stringHexColor);
                             //_serverUIManager.PrintConsole("ID: " + selectedLED.name + " Color: " + stringHexColor);
 
                             //uint number0 = Convert.ToUInt32(stringHexColor, 16);
@@ -108,11 +305,13 @@ namespace GAG.UDPLEDControlSystem
                             string bindedName = lEDNumber + ulongHexColor;
 
                             ulongSendNumber = Convert.ToUInt64(bindedName);
-                            //_serverUIManager.PrintConsole(ulongSendNumber.ToString());
+                            _serverUIManager.PrintConsole(ulongSendNumber.ToString());
                             _driver.BeginSend(NetworkPipeline.Null, _connections[i], out var writer);
                             writer.WriteULong(ulongSendNumber);
                             _driver.EndSend(writer);
                         }
+                        _connections[i].Disconnect(_driver);
+                        _driver.ScheduleFlushSend(default).Complete();
                     }
                     else if (cmd == NetworkEvent.Type.Disconnect)
                     {
@@ -124,5 +323,6 @@ namespace GAG.UDPLEDControlSystem
                 }
             }
         }
+
     }
 }
